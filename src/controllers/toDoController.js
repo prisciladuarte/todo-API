261446f9476b82bb4e35921ee586073c9d24630f
prisciladuarte <duartecostap@gmail.com>
2021-12-09 14:46:34 -0300

    novo

--- src/controllers/toDoController.js
+++ src/controllers/toDoController.js
@@ -1,12 +1,14 @@
 const tarefasJson = require("../models/tarefas.json");
 const fs = require("fs");
+const getAll = (request, response) => {
+  response.status(200).send(tarefasJson);
+};
 
 const createTask = (request, response) => {
   const bodyRequest = request.body;
 
   const novaTarefa = {
     id: Math.random().toString(32).substr(2, 9),
-    outroid: Math.random().toString(32),
     dataInclusao: new Date(),
     descricao: bodyRequest.descricao,
     concluido: false,
@@ -16,11 +18,21 @@ const createTask = (request, response) => {
 
   tarefasJson.push(novaTarefa);
 
-  
+  fs = require("fs");
+  fs.writeFile(
+    "srcmodels\tarefas.json",
+    JSON.stringify(tarefasJson),
+    "utf8",
+    function (err) {
+      if (err) return console.log(err);
+      console.log("eita!");
+    }
+  );
 
-  response.status(201).send(tarefasJson)
+  response.status(201).send(tarefasJson);
 };
 
 module.exports = {
   createTask,
+  getAll
 };
--- src/routes/toDoRoutes.js
+++ src/routes/toDoRoutes.js
@@ -3,6 +3,8 @@ const controller = require("../controllers/toDoController");
 const express = require("express");
 const router = express.Router();
 
+router.get("/", controller.getAll);
+
 router.post("/novo", controller.createTask);
 
 module.exports = router;
